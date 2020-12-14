4dbcde221982bf7579b1446e8772bac8e4e1883b
Santosh Kumar Mohapatra <santosh.mohapatra25@gmail.comm>
2020-12-14 11:11:59 +0530

    modify

--- Startup.cs
+++ Startup.cs
@@ -21,7 +21,7 @@ namespace BulkyBook
         {
             Configuration = configuration;
         }
-
+        //santosh
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
